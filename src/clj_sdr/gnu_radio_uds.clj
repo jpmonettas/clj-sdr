e1d0dae911bb51f0e6eba3e1f36fbb107906f9d0
Juan Monetta <jpmonettas@gmail.com>
2025-07-05 10:33:56 -0300

    core.async refactor

---
+++ src/clj_sdr/gnu_radio_uds.clj
@@ -0,0 +1,99 @@
+(ns clj-sdr.gnu-radio-uds
+  (:require [clojure.core.async :as async])
+  (:import [java.net Socket SocketAddress StandardProtocolFamily]
+           [java.nio ByteBuffer ByteOrder]
+           [java.nio.channels ServerSocketChannel SocketChannel]
+           [java.nio.file Files Paths]
+           [java.time Instant Duration]
+           [java.lang System]
+           [java.net UnixDomainSocketAddress]
+           [org.apache.commons.math3.complex Complex]))
+
+(set! *warn-on-reflection* true)
+
+(def sample-size-bytes 8);; float32 (4 bytes) * 2 (real + imag)
+
+(defn log [& msgs] (println "[gnuradio-uds-block]" (apply str msgs)))
+
+(defn- on-in-conn [^SocketChannel ch dst-ch {:keys [frame-samples-size]}]
+  (log "Got an IN conn")
+  (let [^ByteBuffer buffer (doto (ByteBuffer/allocateDirect (* 2 frame-samples-size))
+                             (.order ByteOrder/LITTLE_ENDIAN))
+        dispatch-complex-samples (fn [^ByteBuffer buf]
+                                   (loop [samples (transient [])]
+                                     (if (>= (.remaining buf) sample-size-bytes)
+                                       (let [real (.getFloat buf)
+                                             imag (.getFloat buf)]
+                                         (recur (conj! samples (Complex. real imag))))
+                                       (async/>!! dst-ch (persistent! samples)))))]
+    (doto (Thread.
+           (fn []
+             (try
+               (while (not (Thread/interrupted))
+                 (let [read-bytes-cnt (.read ch buffer)]
+                   (when (pos? read-bytes-cnt)
+                     (.flip buffer)
+                     (dispatch-complex-samples buffer)
+                     (.clear buffer))))
+               (catch Exception e
+                 (.printStackTrace e)))
+             (log "Reader thread stopped.")))
+      (.start))))
+
+(defn- on-out-conn [^SocketChannel ch src-ch {:keys [frame-samples-size]}]
+  (log "Got an OUT conn")
+  (let [^ByteBuffer buffer (doto (ByteBuffer/allocateDirect (* 2 frame-samples-size))
+                             (.order ByteOrder/LITTLE_ENDIAN))]
+    (doto (Thread.
+           (fn []
+             (while (not (Thread/interrupted))
+               (let [samples-frame (async/<!! src-ch)]
+                 (doseq [^Complex iq-sample samples-frame]
+                   (.putFloat buffer (.getReal iq-sample))
+                   (.putFloat buffer (.getImaginary iq-sample)))
+                 (.flip buffer)
+                 (.write ch buffer)
+                 (.clear buffer)))))
+      (.start))))
+
+(defn gnuradio-uds-block [socket-path opts]
+  (log "Creating UDS socket at " socket-path)
+  (try
+    (let [in-ch (async/chan)
+          out-ch (async/chan)
+          sock-path (Paths/get socket-path (make-array String 0))
+          ssch (ServerSocketChannel/open StandardProtocolFamily/UNIX)
+          _ (Files/deleteIfExists sock-path)
+          _ (.bind ssch (UnixDomainSocketAddress/of sock-path))
+          *threads (atom ())
+          server-thread (doto (Thread.
+                               (fn []
+                                 (try
+                                   (while (not (Thread/interrupted))
+                                     (log "Waiting for connections ...")
+                                     (let [ch (.accept ssch)
+                                           first-byte-buffer (ByteBuffer/allocateDirect 1)
+                                           read-bytes (.read ch first-byte-buffer)]
+                                       (if (= read-bytes 1)
+                                         (case (.get first-byte-buffer 0)
+                                           1 (let [in-thread (on-in-conn ch in-ch opts)]
+                                               (swap! *threads conj in-thread))
+                                           2 (let [out-writer-thread (on-out-conn ch out-ch opts)]
+                                               (swap! *threads conj out-writer-thread)))
+                                         (log "Error reading first byte"))))
+                                   (catch Exception e
+                                     (.printStackTrace e)))
+                                 (log "UDS conn thread stopped")))
+                          (.start))]
+      (swap! *threads conj server-thread)
+      {:in-ch in-ch
+       :out-ch out-ch
+       :stop-fn (fn []
+                  (doseq [^Thread th @*threads]
+                    (.interrupt th))
+                  (async/close! in-ch)
+                  (async/close! out-ch)
+                  (.close ssch)
+                  (.interrupt server-thread))})
+    (catch Exception e
+      (.printStackTrace e))))
--- src/clj_sdr/main.clj
+++ src/clj_sdr/main.clj
@@ -1,118 +1,59 @@
 (ns clj-sdr.main
-  (:import [java.net Socket SocketAddress StandardProtocolFamily]
-           [java.nio ByteBuffer ByteOrder]
-           [java.nio.channels ServerSocketChannel SocketChannel]
-           [java.nio.file Files Paths]
-           [java.time Instant Duration]
-           [java.lang System]
-           [java.net UnixDomainSocketAddress]
-           [org.apache.commons.math3.complex Complex]))
+  (:require [clojure.core.async.flow :as flow]
+            [clojure.core.async :as async]
+            [clj-sdr.gnu-radio-uds :refer [gnuradio-uds-block]]))
 
 
 (set! *warn-on-reflection* true)
 
-(defn start-uds-socket-server [socket-path {:keys [on-in-conn on-out-conn]}]
-  (println "Creating UDS socket at " socket-path)
-  (let [sock-path (Paths/get socket-path (make-array String 0))
-        ssch (ServerSocketChannel/open StandardProtocolFamily/UNIX)
-        _ (Files/deleteIfExists sock-path)
-        _ (.bind ssch (UnixDomainSocketAddress/of sock-path))
-        server-thread (doto (Thread.
-                             (fn []
-                               (try
-                                 (while (not (Thread/interrupted))
-                                   (println "Waiting for connections ...")
-                                   (let [ch (.accept ssch)
-                                         first-byte-buffer (ByteBuffer/allocateDirect 1)
-                                         read-bytes (.read ch first-byte-buffer)]
-                                     (if (= read-bytes 1)
-                                       (case (.get first-byte-buffer 0)
-                                         1 (on-in-conn ch)
-                                         2 (on-out-conn ch))
-                                       (println "Error reading first byte"))))
-                                 (catch Exception e
-                                   (.printStackTrace e)))
-                               (println "UDS conn thread stopped")))
-                        (.start))
-        stop-server (fn []
-                      (.close ssch)
-                      (.interrupt server-thread))]
-    stop-server))
+(def frame-samples-size 4096)
 
-(def sample-size-bytes 8) ;; float32 (4 bytes) * 2 (real + imag)
-(def stop-reader-thread nil)
-(def stop-server nil)
-(def write-sample nil)
+;; (defn scaler
+;;   ([] {:outs {:samples "A vector of Complex IQ samples"}})
+;;   ([_] {::flow/in-ports {:samples-batch gnu-radio-samples-batch-ch}})
+;;   ([state _] state)
+;;   ([state ch-id samples-batch]
+;;    [state (into [] (map (fn [^Complex iq-sample]
+;;                           (.multiply iq-sample 2)))
+;;                 samples-batch)]))
 
-(defn process-sample [^Complex iq-sample]
-  (when write-sample
-    (write-sample (.getReal iq-sample) (.getImaginary iq-sample)))
-  #_(println "@@@ GOT " iq-sample))
+;; (defn gnu-radio-sender
+;;   ([] {:ins {:samples "A vector of Complex IQ samples"}})
+;;   ([_] {::flow/out-ports {:samples-batch gnu-radio-samples-batch-ch}})
+;;   ([state _] state)
+;;   ([state ch-id samples-batch]
+;;    [state (into [] (map (fn [^Complex iq-sample]
+;;                           (.multiply iq-sample 2)))
+;;                 samples-batch)]))
 
-(defn on-in-conn [^SocketChannel ch]
-  (println "Got an IN conn")
-  (let [^ByteBuffer buffer (doto (ByteBuffer/allocateDirect 4096)
-                             (.order ByteOrder/LITTLE_ENDIAN))
-        dispatch-complex-samples (fn [^ByteBuffer buf]
-                                   (loop []
-                                     (when (>= (.remaining buf) sample-size-bytes)
-                                       (let [real (.getFloat buf)
-                                             imag (.getFloat buf)]
-                                         (process-sample (Complex. real imag))
-                                         (recur)))))
-        reader-thread (Thread.
-                       (fn []
+;; (def system-graph
+;;   (flow/create-flow
+;;    {:procs
+;;     {:scaler {:proc (flow/process scaler)}
+;;      :gnu-radio-sender   {:proc (flow/process gnu-radio-sender)}}
 
-                         (try
-                           (while (not (Thread/interrupted))
-                            (let [read-bytes (.read ch buffer)]
-                              (when (pos? read-bytes)
-                                (.flip buffer)
-                                (dispatch-complex-samples buffer)
-                                (.clear buffer))))
-                           (catch Exception e
-                             (.printStackTrace e)))
-                         (println "Reader thread stopped.")))]
-    (alter-var-root #'stop-reader-thread
-                    (constantly (fn [] (.interrupt reader-thread))) )
-    (.start reader-thread)))
+;;     :conns
+;;     [[[:scaler :samples]         [:gnu-radio-sender  :samples]]]}))
 
-(defn on-out-conn [^SocketChannel ch]
-  (println "Got an OUT conn")
-  (let [^ByteBuffer buffer (doto (ByteBuffer/allocateDirect 4096)
-                             (.order ByteOrder/LITTLE_ENDIAN))
-        write-buf-to-ch (fn []
-                          (locking buffer
-                            (.flip buffer)
-                            (.write ch buffer)
-                            (.clear buffer)))]
-    (alter-var-root #'write-sample
-                    (constantly
-                     (fn [I Q]
-                       (locking buffer
-                         (if (>= (.remaining buffer) 2)
-                           (do
-                             (.putFloat buffer I)
-                             (.putFloat buffer Q))
-                           (do
-                             (write-buf-to-ch)
-                             (.putFloat buffer I)
-                             (.putFloat buffer Q)))))))))
-
-(defn stop-all []
-  (stop-server)
-  (when stop-reader-thread
-    (stop-reader-thread))
-  (alter-var-root #'write-sample (constantly nil)))
+(def stop-all nil)
+(def gr-in-ch nil)
+(def gr-out-ch nil)
 
 (defn -main [& args]
-  (let [stop-fn (start-uds-socket-server "/tmp/clj_sdr.sock"
-                                         {:on-in-conn on-in-conn
-                                          :on-out-conn on-out-conn})]
-    (alter-var-root #'stop-server (constantly stop-fn))))
+  (let [{:keys [in-ch out-ch stop-fn]} (gnuradio-uds-block "/tmp/clj_sdr.sock"
+                                                           {:frame-samples-size frame-samples-size})]
+    (alter-var-root #'stop-all (constantly stop-fn))
+    (alter-var-root #'gr-in-ch (constantly in-ch))
+    (alter-var-root #'gr-out-ch (constantly out-ch))
+    (async/pipeline 1 gr-out-ch (map identity) gr-in-ch)))
 
 (comment
-
+  (async/pipeline 1 gr-out-ch (map identity) gr-in-ch)
+  (async/pipeline 1 gr-out-ch (map (fn [frame]
+                                     (into [] (map (fn [^Complex s]
+                                                     (.multiply s 2.0)))
+                                           frame)))
+                  gr-in-ch)
   (-main)
   (stop-all)
   )
